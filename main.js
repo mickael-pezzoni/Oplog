d0ea245d7eb350c7bb340e7532c5ebf4657e60dc
mickael2407 <pezzoni.mickael@gmail.com>
2019-10-03 11:38:41 +0200

    test notif

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const MongoOplog = require('mongo-oplog');
+const notifier = require('node-notifier');
+const beep = require('beepbeep')
+
 const USER = 'oplogger';
 const PWD = '***REMOVED***';
 const HOST = '***REMOVED***';
@@ -9,16 +12,22 @@ const MEMBERS = [
   {host: HOST, port: '6413'}
 ].map(_member => _member.host + ':' + _member.port).join(',');
 
-const oplog = MongoOplog(`mongodb://${USER}:${PWD}@${MEMBERS}/local?authSource=admin&replicaSet=rs0` , 'Cefirc');
+const oplog = MongoOplog(`mongodb://${USER}:${PWD}@${MEMBERS}/local?authSource=admin&replicaSet=rs0` , 'MellispheraTest');
 
 oplog.tail();
 
 oplog.on('op', data => {
-  console.log(data);
+//  console.log(data);
 });
 
 oplog.on('insert', doc => {
   console.log(doc);
+  beep(3);
+  notifier.notify({
+    title: 'Alert',
+    message: JSON.stringify(doc.o),
+    sound: true, // Only Notification Center or Windows Toasters
+  });
 });
 
 oplog.on('update', doc => {
